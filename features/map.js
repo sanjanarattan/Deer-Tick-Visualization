afb3428cbbb450748384a9495a14f0a6f5fd43c9
Sanjana Rattan <sprasad10@dons.usfca.edu>
2025-03-29 16:02:06 -0700

    Built map of New York Counties

--- features/map.js
+++ features/map.js
@@ -1,5 +1,7 @@
-const width = 1200;
+const width = 1600;
 const height = 800;
+
+const SCALE = 8000;
 
 function load(){
     d3.json("https://services6.arcgis.com/EbVsqZ18sv1kVJ3k/ArcGIS/rest/services/NYS_Civil_Boundaries/FeatureServer/2/query?f=geojson&where=1=1")
@@ -12,7 +14,29 @@ function load(){
 }
 
 function draw(data){
-    return
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", width)
+                  .attr("height", height)
+
+    var gfg = d3.geoAlbers()
+        .scale(SCALE)
+        .translate([width / 2 - 2100, height / 2 + 800])
+        
+    const path = d3.geoPath()
+        .projection(gfg)
+    
+    
+    svg.selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+        .attr("d", path) 
+        .attr("fill", "#FFF")
+        .attr("stroke", "#000")
+        .attr("stroke-width", 1)
+
+
 }
 
 load()
